0d1e69f3d6b21ff25aa2e3c9577a52d27b9714c9
yudi1523 <565527652@qq.com>
2018-07-13 20:31:19 +0800

    主页完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -10,5 +10,38 @@ $(document).ajaxStop(function () {
   setTimeout(function () {
     //关闭进度条
     NProgress.done();
-  },1000);
+  },500);
+})
+
+//二级分类显示与隐藏功能
+$('.child ').prev().on('click',function () {
+  $(this).next().slideToggle();
+})
+//侧边栏显示与隐藏
+$('.icon_menu').on('click',function () {
+  console.log(11);
+  $('.lt_aside').toggleClass('now');
+  $('.lt_main').toggleClass('now');
+  $(".lt_topbar").toggleClass('now');
+})
+
+//退出功能
+$('.icon_logout').on('click',function () {
+
+  console.log(111);
+  $('#logoutModal').modal('show');
+})
+$('.btn_logout').on('click',function () {
+  console.log(111);
+  $.ajax({
+    type:'get',
+    url:'/employee/employeeLogout',
+    success:function (info) {
+     
+      if(info.success){
+        //退出成功
+        location.href = 'login.html';
+      }
+    }
+  })
 })
